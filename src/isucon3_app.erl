11eef89d8ccbdbff22108008dda5946128236b43
Kuniyoshi Kouji <kuniyoshi@cpan.com>
2013-08-07 02:13:22 +0900

    static

--- src/isucon3_app.erl
+++ src/isucon3_app.erl
@@ -3,16 +3,16 @@
 -export([start/2, stop/1]).
 
 start(_Type, _Args) ->
-    Dispatch = [
-                {'_', [
-                       {['...'], cowboy_static, [
-                                                 {directory, {priv_dir, static, []}},
-                                                 {mimetypes, {fun mimetypes:path_to_mimes/2, default}}
-                                                ]}
-                      ]}
-               ],
+    Dispatch = cowboy_router:compile([
+                                      {'_', [
+                                             {"/static/[...]", cowboy_static, [
+                                                                        {directory, {priv_dir, isucon3, []}},
+                                                                        {mimetypes, {fun mimetypes:path_to_mimes/2, default}}
+                                                                       ]}
+                                            ]}
+                                     ]),
     {ok, _} = cowboy:start_http(http, 100, [{port, 8080}], [
-                                                            {dispatch, Dispatch}
+                                                            {env, [{dispatch, Dispatch}]}
                                                            ]),
     isucon3_sup:start_link().
 
--- src/isucon3_sup.erl
+++ src/isucon3_sup.erl
@@ -3,11 +3,9 @@
 -export([start_link/0]).
 -export([init/1]).
 
--define(CHILD(I, Type), {I, {I, start_link, []}, permanent, 5000, Type, [I]}).
-
 start_link() ->
     supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
 init([]) ->
     Procs = [],
-    {ok, {{one_for_one, 5, 10}, Procs}}.
+    {ok, {{one_for_one, 10, 10}, Procs}}.
